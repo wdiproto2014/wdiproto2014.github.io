b15a3ddabca1906b8f8a4a787bf517fdf1905148
Ben Karl <benrkarl@gmail.com>
2014-06-28 16:44:55 -0400

    add student view

---
+++ javascripts/backbone/views/StudentView.js
@@ -0,0 +1,10 @@
+var StudentView = Backbone.View.extend({
+  template: _.template($('#student-template')),
+  initialize: function(){
+    this.listenTo(this.model, 'change', this.render)
+  },
+  render: function(){
+    this.$el.html(this.template(this.model.attributes))
+    return this;
+  }
+})
